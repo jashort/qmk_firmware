5e3e3adbac1fb507ffadba5fad2bbc79d3737112
Jason Short <jason@sheersky.com>
2023-03-27 20:14:46 -0700

    Fix LCtrl

--- keyboards/splitkb/kyria/keymaps/jashort/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jashort/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSLS,
      GUI_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST ,MOD_LCTL, ALT_SPC, KC_BSPC, NAV   ,     SYM    , KC_DELETE , KC_ENTER, KC_RALT, KC_APP
+                                ADJUST , KC_LCTL, ALT_SPC, KC_BSPC, NAV   ,     SYM    , KC_DELETE , KC_ENTER, KC_RALT, KC_APP
     ),
 
 /*
