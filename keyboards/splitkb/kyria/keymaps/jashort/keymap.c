1af95107c4bfc2deb1f7f7b474e2647e2f1bfcc6
Jason Short <jason@sheersky.com>
2023-04-01 20:56:03 -0700

    Move FKeys, make it a one shot layer

--- keyboards/splitkb/kyria/keymaps/jashort/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jashort/keymap.c
@@ -29,7 +29,7 @@ enum layers {
 
 #define SYM      MO(_SYM)
 #define NAV      MO(_NAV)
-#define FKEYS    MO(_FUNCTION)
+#define FKEYS    OSL(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 
 #define GUI_ESC  MT(MOD_LGUI, KC_ESC)
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+------|
  * | LSFT |  Z   |  X   |  C   |  V   |  B   |  [   | CAPS |  |FKEYS |  ]   |  N   |  M   | , >  | . <  | / ?  | RSFT |
  * `------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+------'
- *                      | LALT | LCTL | SPC  | BSPC | NAV  |  |DELETE| SYM  |ENTER |  NAV | RALT |
+ *                      | LALT | LCTL | SPC  | BSPC |FKEYS |  |DELETE| SYM  |ENTER |  NAV | RALT |
  *                      |      |      |      |      |      |  |      |      |      |      |      |
  *                      `----------------------------------'  `----------------------------------'
 
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,    KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSLS,
      GUI_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,    KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,    KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-     KC_LALT , KC_LCTL, KC_SPACE, KC_BSPC, NAV   ,     KC_DELETE , SYM , KC_ENTER, NAV , KC_RALT
+     KC_LALT , KC_LCTL, KC_SPACE, KC_BSPC, FKEYS,     KC_DELETE , SYM , KC_ENTER, NAV , KC_RALT
     ),
 
 /*
