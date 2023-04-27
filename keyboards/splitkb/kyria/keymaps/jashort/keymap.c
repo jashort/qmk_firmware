b7a88552c7f263055bd9e6f471dbb4e567d510b6
Jason Short <jason@sheersky.com>
2023-04-26 22:22:04 -0700

    Remove brackets on base, add Screenshot Copy

--- keyboards/splitkb/kyria/keymaps/jashort/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jashort/keymap.c
@@ -37,6 +37,7 @@ enum layers {
 
 #define GUI_ESC  MT(MOD_LGUI, KC_ESC)
 //#define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
+#define SCNSHT   LGUI(LSFT(LCTL(KC_4)))
 #define ENT_SYM  LT(SYM, KC_ENTER)
 
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
@@ -52,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                              |------+------+------+------+------+------|
  * |GUI_ES|  A   |  S   |  D   |  F   |  G   |                              |  H   |  J   |  K   |  L   | ; :  |   '  |
  * |------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+------|
- * | LSFT |  Z   |  X   |  C   |  V   |  B   |  [   | CAPS |  |FKEYS |  ]   |  N   |  M   | , <  | . >  | / ?  | RSFT |
+ * | LSFT |  Z   |  X   |  C   |  V   |  B   |SCNSHT| CAPS |  |FKEYS |      |  N   |  M   | , <  | . >  | / ?  | RSFT |
  * `------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+------'
  *                      | LALT | LCTL |SPACE | BSPC |FKEYS |  |DELETE|FKEYS |ENT_SY| NAV  | RALT |
  *                      |      |      |      |      |      |  |      |      |      |      |      |
@@ -63,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,    KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSLS,
      GUI_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,    KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOTE,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,    KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,SCNSHT,KC_CAPS,     FKEYS  , _______, KC_N,    KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
      KC_LALT , KC_LCTL, KC_SPACE, KC_BSPC, FKEYS,     KC_DELETE , FKEYS  , ENT_SYM, NAV, KC_RALT
     ),
 
