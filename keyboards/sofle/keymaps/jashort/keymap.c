3253f7b07910c8a959d3595e96c613fe3a0f2666
Jason Short <jason@sheersky.com>
2023-07-17 20:12:02 -0700

    Add plus key, renamed layer keys
    Layer keys should show up better in the visualizer

--- keyboards/sofle/keymaps/jashort/keymap.c
+++ keyboards/sofle/keymaps/jashort/keymap.c
@@ -16,7 +16,9 @@ enum custom_keycodes {
     KC_DLINE,
     WRD_L = LOPT(KC_LEFT),
     WRD_R = LOPT(KC_RIGHT),
-    ENT_SYM = LT(_LOWER, KC_ENTER)
+    ENT_LOW = LT(_LOWER, KC_ENTER),
+    LOWER = MO(_LOWER),
+    RAISE = MO(_RAISE)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DEL,
-        KC_LCTL,KC_LOPT,KC_LCMD, KC_SPC , KC_BSPC,     MO(_LOWER), ENT_SYM, MO(_RAISE), KC_RALT, KC_RGUI
+        KC_LCTL,KC_LOPT,KC_LCMD, KC_SPC , KC_BSPC,         LOWER, ENT_LOW, RAISE, KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -51,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * | Shift|   [  |  ]   |   {  |  }   | - _  |-------|    |-------|   _  | = +  |   ;  |   :  |   \  | Shift|
+ * | Shift|   [  |  ]   |   {  |  }   | - _  |-------|    |-------|   _  | = +  |   +  |   :  |   \  |  Del |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCtl | LOpt | LCmd | SPACE | / BKSP /       \LOWER \  |ENTER |RAISE | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |LOWER |      |      |      |
@@ -61,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
   _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_MINS, _______,       _______, KC_UNDS, KC_EQL, KC_SCLN, KC_COLN, KC_BSLS, _______,
+  _______,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_MINS, _______,       _______, KC_UNDS, KC_EQL, KC_PLUS, KC_COLN, KC_BSLS, _______,
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* RAISE
@@ -72,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  | LAt  | LCtl |LShift|      | Caps |-------.    ,-------| - _  | Left | Down | Rigth|  Del | Bspc |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * | Shift|   [  |  ]   |   {  |  }   | - _  |-------|    |-------|  _  | = +  |  LStr | LEnd |      | Shift|
+ * | Shift|   [  |  ]   |   {  |  }   | - _  |-------|    |-------|   _  | = +  |   +  | LStr | LEnd | Del  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCtl | LOpt | LCmd | SPACE | / BKSP /       \LOWER \  |ENTER |RAISE | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |LOWER |      |      |      |
@@ -82,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______ , _______ , _______ , _______ , _______,                      KC_PGUP ,  _______  , _______,  _______ ,  _______ ,_______,
   _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                       KC_PGDN , WRD_L,   KC_UP, WRD_R ,KC_DLINE, KC_BSPC,
   _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_MINS,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_MINS, _______,       _______,  KC_UNDS,  KC_EQL, KC_LSTRT, KC_LEND,   XXXXXXX, _______,
+  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_MINS, _______,       _______,  KC_UNDS,  KC_EQL, KC_PLUS, KC_LSTRT, KC_LEND, _______,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 )
 };
