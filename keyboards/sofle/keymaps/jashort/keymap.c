3b5839c458a99e8f5b3b8bc3f57bab7b148d7da7
Jason Short <jason@sheersky.com>
2023-10-10 20:40:22 -0700

    Shift under right thumb

--- keyboards/sofle/keymaps/jashort/keymap.c
+++ keyboards/sofle/keymaps/jashort/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | DEL  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCtl | LOpt | LCmd | SPACE | /LOWER /       \RAISE \  |ENTER |RAISE | RAlt | RGUI |
+ *            | LCtl | LOpt | LCmd | SPACE | /LOWER /       \SHIFT \  |ENTER |RAISE | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DEL,
-        KC_LCTL,KC_LOPT,KC_LCMD, KC_SPC , KC_BSPC,         LOWER, ENT_LOW, RAISE, KC_RALT, KC_RGUI
+        KC_LCTL,KC_LOPT,KC_LCMD, KC_SPC , KC_BSPC,         KC_RSFT, ENT_LOW, RAISE, KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * | Shift|   [  |  ]   |   {  |  }   | - _  |-------|    |-------|   _  | = +  |   +  |   :  |   \  |  Del |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCtl | LOpt | LCmd | SPACE | / BKSP /       \LOWER \  |ENTER |RAISE | RAlt | RGUI |
+ *            | LCtl | LOpt | LCmd | SPACE | / BKSP /       \SHIFT \  |ENTER |RAISE | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |LOWER |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * | Shift|   [  |  ]   |   {  |  }   | - _  |-------|    |-------|   _  | = +  |   +  | LStr | LEnd | Del  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCtl | LOpt | LCmd | SPACE | / BKSP /       \LOWER \  |ENTER |RAISE | RAlt | RGUI |
+ *            | LCtl | LOpt | LCmd | SPACE | / BKSP /       \SHIFT \  |ENTER |RAISE | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |LOWER |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
