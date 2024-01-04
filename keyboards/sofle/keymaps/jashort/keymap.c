3964d125f4403dc8570522503fb63184d1870f8a
Jason Short <jason@sheersky.com>
2024-01-04 14:47:29 -0800

    Update Sofle to match Helix layout

--- keyboards/sofle/keymaps/jashort/keymap.c
+++ keyboards/sofle/keymaps/jashort/keymap.c
@@ -22,7 +22,8 @@ enum custom_keycodes {
     WRK1 = LCTL(KC_1),
     WRK2 = LCTL(KC_2),
     WRK3 = LCTL(KC_3),
-    SSHOT = LCTL(SGUI(KC_4))
+    SSHOT = LCTL(SGUI(KC_4)),
+    SSAV = SGUI(KC_4)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -33,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \|  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |Ctl/Esc|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | DEL  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -45,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  LCTL_T(KC_ESC),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DEL,
         KC_LCTL,KC_LOPT,KC_LCMD, KC_SPC , KC_BSPC,         KC_RSFT, ENT_LOW, RAISE, KC_RALT, KC_RGUI
 ),
@@ -66,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLS,
-        _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_MINS, _______,     _______, KC_UNDS,  KC_EQL, KC_PLUS, XXXXXXX, XXXXXXX, _______,
+         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
+        _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_UNDS, _______,     _______, KC_MINS,  KC_EQL, KC_PLUS, XXXXXXX, XXXXXXX, _______,
                        _______, _______, _______, _______, _______,               _______, _______, _______, _______, _______
 ),
 /* RAISE
@@ -88,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TILD,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  , XXXXXXX,                      KC_PGUP,   WRD_L,   KC_UP,  WRD_R , KC_HOME, XXXXXXX,
         KC_GRV ,  KC_F5  ,  KC_F6  ,  KC_F7  ,  KC_F8  , KC_CAPS,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_LEND, XXXXXXX,
-        _______,  KC_F9  ,  KC_F10 ,  KC_F11 ,  KC_F12 , KC_MINS, _______,    _______,  KC_UNDS,   WRK1,    WRK2,    WRK3,    SSHOT, _______,
+        _______,  KC_F9  ,  KC_F10 ,  KC_F11 ,  KC_F12 , KC_UNDS, _______,    _______, KC_MINS, XXXXXXX, XXXXXXX,    SSAV,    SSHOT, _______,
                             _______,  _______,  _______, _______, _______,    _______,  _______, _______, _______, _______
 )
 };
