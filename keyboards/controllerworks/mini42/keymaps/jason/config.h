bfa1b1344e44248628ebd0f2fcfdc5f9ed4dc296
Jason Short <jason@sheersky.com>
2024-02-11 12:06:02 -0800

    Enable permissive hold

--- keyboards/controllerworks/mini42/keymaps/jason/config.h
+++ keyboards/controllerworks/mini42/keymaps/jason/config.h
@@ -15,6 +15,8 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define PERMISSIVE_HOLD
+
 #pragma once
 #ifdef RGB_MATRIX_ENABLE
 //#    define SPLIT_TRANSPORT_MIRROR
