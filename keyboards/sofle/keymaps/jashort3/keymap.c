709716e5a630b9995f28c0b8caf2e4b5fdcc8fed
Jason Short <jason@sheersky.com>
2024-03-27 20:35:57 -0700

    Flip nav and sym layers

--- keyboards/sofle/keymaps/jashort3/keymap.c
+++ keyboards/sofle/keymaps/jashort3/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
         XXXXXXX,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, XXXXXXX,
         XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
-                       XXXXXXX,XXXXXXX, LA_NAV, KC_SPC , KC_BSPC,             KC_RSFT, KC_SPC, LA_SYM, XXXXXXX, XXXXXXX
+                       XXXXXXX,XXXXXXX, LA_SYM, KC_SPC , KC_BSPC,             KC_RSFT, KC_ENT, LA_NAV, XXXXXXX, XXXXXXX
         ),
 
     [SYM] = LAYOUT(
